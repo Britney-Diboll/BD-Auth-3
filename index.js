8779c0a6813d8974a7ccfc9dfaf250ab828071ca
Britney Diboll <36859804+Britney-Diboll@users.noreply.github.com>
2023-02-28 11:40:48 -0500

    added angularJs

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-netlifyIdentity.on('init', () => {
+let app = angular.module('myApp', ['ngRoute']);
+
+app.config(function () {
+  $routeProvider
+  .when("/", {
+      templateUrl: "index.html",
+  })
+  .when("/index", {
+      templateUrl: "index.html",
+  })
+  .when("/landing", {
+      templateUrl: "landing.html",
+  })
+
+
+})
+.controller('myCtrl', function ($scope) {
+
+  netlifyIdentity.on('init', () => {
     initUser = netlifyIdentity.currentUser();
     console.log(initUser);
   });
@@ -7,7 +25,7 @@ netlifyIdentity.on('init', () => {
   
   netlifyIdentity.on('login', () => {
     if (initUser == null) {
-        window.location.replace('landing');
+        window.location.replace('#!landing');
     }
     netlifyIdentity.close();
   });
@@ -16,3 +34,7 @@ netlifyIdentity.on('init', () => {
     netlifyIdentity.close();
     window.location.replace('/');
   });
+
+
+});
+
